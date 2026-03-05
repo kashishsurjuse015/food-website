ea5e5f8aedcc3a1879686b36952295483cb2be6a
kashishsurjuse015 <kashishsurjuse45@gmail.com>
2026-03-05 18:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,29 @@ def blog():
 
 def contact():
     return render_template('contact.html')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
- app.run(debug=True)  
+    app.run(debug=True)
