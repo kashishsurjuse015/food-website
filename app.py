0d9d6ded9833e15ebd6847f859f39c41d3bc86ee
kashishsurjuse015 <kashishsurjuse45@gmail.com>
2026-03-07 19:31:43 +0530

    Update menu route to include 10 food items

--- app.py
+++ app.py
@@ -24,22 +24,32 @@ def home():
     return render_template('index.html')
 
 # 2. MENU ROUTE (Yahan aapka menu load hoga)
+# --- Ye portion app.py mein niche paste karein ---
+
 @app.route('/menu')
 def menu():
-    # Purana 3 items wala code hata kar ye 10 items wala daalye
     food_items = [
         {"_id": "1", "name": "Special Chicken Biryani", "price": 250, "description": "Slow-cooked aromatic rice with tender chicken spices.", "image_url": "/static/01.jpg", "stripe_url": "#"},
         {"_id": "2", "name": "Veg Hyderabadi Biryani", "price": 180, "description": "Fresh vegetables mixed with basmati rice and saffron.", "image_url": "/static/06.jpg", "stripe_url": "#"},
         {"_id": "3", "name": "Paneer Tikka Biryani", "price": 220, "description": "Grilled paneer cubes layered with spicy biryani rice.", "image_url": "/static/02.jpg", "stripe_url": "#"},
         {"_id": "4", "name": "Mutton Dum Biryani", "price": 350, "description": "Rich flavors of mutton with long grain basmati rice.", "image_url": "/static/03.jpg", "stripe_url": "#"},
-        {"_id": "5", "name": "Chicken Lollipop", "price": 200, "description": "Deep fried chicken appetizers with schezwan sauce.", "image_url": "/static/04.jpg", "stripe_url": "#"},
-        {"_id": "6", "name": "Egg Biryani", "price": 160, "description": "Spiced eggs served with fragrant biryani rice.", "image_url": "/static/05.jpg", "stripe_url": "#"},
-        {"_id": "7", "name": "Prawns Biryani", "price": 300, "description": "Delicious prawns cooked in traditional masala style.", "image_url": "/static/07.jpg", "stripe_url": "#"},
-        {"_id": "8", "name": "Butter Chicken", "price": 280, "description": "Creamy tomato gravy with grilled chicken pieces.", "image_url": "/static/08.jpg", "stripe_url": "#"},
-        {"_id": "9", "name": "Dal Tadka", "price": 140, "description": "Yellow lentils tempered with ghee and spices.", "image_url": "/static/09.jpg", "stripe_url": "#"},
-        {"_id": "10", "name": "Gulab Jamun", "price": 60, "description": "Sweet dessert balls soaked in sugar syrup.", "image_url": "/static/10.jpg", "stripe_url": "#"}
+        {"_id": "5", "name": "Chicken Lollipop", "price": 200, "description": "Deep fried chicken appetizers with sauce.", "image_url": "/static/04.jpg", "stripe_url": "#"},
+        {"_id": "6", "name": "Egg Biryani", "price": 160, "description": "Spiced eggs served with fragrant rice.", "image_url": "/static/05.jpg", "stripe_url": "#"},
+        {"_id": "7", "name": "Prawns Biryani", "price": 300, "description": "Delicious prawns cooked in masala style.", "image_url": "/static/07.jpg", "stripe_url": "#"},
+        {"_id": "8", "name": "Butter Chicken", "price": 280, "description": "Creamy tomato gravy with chicken pieces.", "image_url": "/static/08.jpg", "stripe_url": "#"},
+        {"_id": "9", "name": "Dal Tadka", "price": 140, "description": "Yellow lentils tempered with ghee.", "image_url": "/static/09.jpg", "stripe_url": "#"},
+        {"_id": "10", "name": "Gulab Jamun", "price": 60, "description": "Sweet dessert balls in sugar syrup.", "image_url": "/static/10.jpg", "stripe_url": "#"}
     ]
-    return render_template('menu.html', food_items=food_items)    
+    return render_template('menu.html', food_items=food_items)
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 # 3. OTHER ROUTES
 @app.route('/login', methods=['GET', 'POST'])
